37d974fc8841affa4bb4d2a0d902f9d83f6f1484
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2022-08-29 23:07:10 +0500

    refactoring file icon

--- src/components/FileIcon/FileIcon.tsx
+++ src/components/FileIcon/FileIcon.tsx
@@ -1,19 +1,26 @@
-import type { Property } from 'csstype';
-
 export type FileIconProps = {
   /** Base color. */
-  fill: Property.Fill;
+  fill: string;
 
   /** Width and height. */
   size: string | number;
+
+  /** List of CSS classes. */
+  className?: string;
 };
 
-export function FileIcon({ fill, size }: FileIconProps) {
+export function FileIcon({ size, className, fill }: FileIconProps) {
   return (
-    <svg viewBox="0 0 225 225" width={size} height={size}>
+    <svg
+      viewBox="0 0 225 225"
+      width={size}
+      height={size}
+      className={className}
+      aria-hidden="true"
+    >
       <path
         d="m37.494 9.6172a8 8 0 0 0-8 8v189.95a8 8 0 0 0 8 8h149.28a8 8 0 0 0 8-8v-95.07-46.543l-45.721-56.34h-111.56zm14.551 75.367h115.7 4.2969v4.375 5.625h-120v-10zm0 28.969h120v10h-120v-10zm0 29.088h120v10h-120v-10zm0 29.176h60v10h-60v-10z"
-        style={{ fill }}
+        fill={fill}
       />
       <path
         d="m149.06 65.957 18.691 19.027h4.2969v4.375l22.732 23.141v-46.543z"
